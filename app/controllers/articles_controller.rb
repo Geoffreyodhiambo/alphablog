b6d83971d37d0765594fdbb6932f88238e668429
GEOFFREYe <geoffreyodhiambo98@gmail.comm>
2020-06-10 15:42:00 +0300

    create article on browser

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,14 @@ class ArticlesController < ApplicationController
 
   end
 
+def new
+end
+
+def create
+  @article = Article.new(params.require(:article).permit(:title, :description))
+  @article.save
+  redirect_to @article
+end
 
 
 end
