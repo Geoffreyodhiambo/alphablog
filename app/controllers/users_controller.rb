0ea2f92d60df0bae99c2ccf4b432ccf2fa3ef91c
GEOFFREYe <geoffreyodhiambo98@gmail.comm>
2020-07-06 15:35:13 +0300

    show user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,41 @@
 class UsersController < ApplicationController
 
-    def new
-      @user = User.new
-    end
-    def edit
-      @user = User.find(params[:id])
-      
-    end
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
 
-    def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-        flash[:notice] = "Your details was successfully updated"
-        redirect_to articles_path
-      else
-        render 'edit'
-      end
-    end
+  def new
+    @user = User.new
+  end
 
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        flash[:notice] = "Welcome to Geoffrey Blog #{@user.username}, you have succesfully sign up"
-        redirect_to articles_path
-      else
-        render 'new'
-      end
+  def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
     end
-    
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Geoffrey Blog #{@user.username}, you have successfully signed up"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-
-  end
+  
+end
