04dcb8aeb5372fc381453c5dbced8d5410acb110
Geoffrey <geoffreyodhiambo98@gmail.com>
2021-01-14 14:34:19 +0300

    update Gemlock file, category.rb and test/controller

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -17,22 +17,13 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     get new_category_url
     assert_response :success
   end
-
-  test "should create category" do
-    sign_in_as(@admin_user)
-    assert_difference('Category.count', 1) do
-      post categories_url, params: { category: { name: "Travel" } }
-    end
-
-    assert_redirected_to category_url(Category.last)
-  end
-
-  test "should not create category if not admin" do
+  
+ test "should not create category if not admin" do
     assert_no_difference('Category.count') do
       post categories_url, params: { category: { name: "Travel" } }
     end
 
-    assert_redirected_to categories_url
+      assert_redirected_to categories_url
   end
 
   test "should show category" do
